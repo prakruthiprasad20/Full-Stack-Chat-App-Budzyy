8d16c26887458b041198c1aad876d100eb5f9020
Prakruthi Prasad <prakruthiprasad19@gmail.com>
2025-12-27 18:56:41 +0530

    error debug

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,9 +26,14 @@ app.use("/api/messages", messageRoutes)
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+  
+  app.get("/(.*)", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../frontend/dist/index.html")
+  );
+});
+
+
 }
 
 // Start server with Socket.IO support
